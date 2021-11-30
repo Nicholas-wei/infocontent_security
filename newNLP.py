266ed5e94c70b989965be315db8658cbb1b37707
zhuhai <451554798@qq.com>
2021-11-30 20:07:52 +0800

    将函数封装，每个函数写了概要注释

--- newNLP.py
+++ newNLP.py
@@ -15,7 +15,7 @@ class data:
 data_path = "D:\\data_analysis\\11.27"
 NLPd_path = "D:\\data_analysis\\zhihuPJanalysis\\"
 
-
+# 检查是否为回答，不是则是文章
 def check_answer(file):
     f = open(file,'r',encoding='UTF-8')
     s = f.readline()
@@ -24,7 +24,7 @@ def check_answer(file):
     else:
         return False
 
-
+# 检查是否是元宇宙相关
 def check_meta(string):
     if string.find("元宇宙") == -1 and string.find("Meta") == -1 and string.find("meta") == -1:
         return False
@@ -32,6 +32,7 @@ def check_meta(string):
         return True
 
 
+# 获取所有文件名
 def get_all_file(data_path):
     dirs = os.listdir(data_path)
     files = []
@@ -41,39 +42,33 @@ def get_all_file(data_path):
     return files
 
 
-def snowNLP_analysis(files):
-    answers = []
-    articles = []
-    for file in files:
-        if check_answer(file):
-            openf = open(file, 'r', encoding='UTF-8')
-            raw_data = openf.readlines()
-            i = 0
-            while i < len(raw_data):
-                if raw_data[i][0] == '$':
-                    if check_meta(raw_data[i+4]):
-                        createtime = raw_data[i][1:-1].strip()
-                        enduptime = raw_data[i+1].strip()
-                        sexuality = raw_data[i+2].strip()
-                        linkdata  = raw_data[i+3].strip()
-                        string    = raw_data[i+4].strip()
-                        answers.append({'create time':createtime, 'endup time':enduptime,'sexuality':sexuality,'link':linkdata,'answer':string})
-                        i += 5
-                    else:
-                        i += 1
+# 获取所有文件数据
+def get_data(file,answers,articles):
+    openf = open(file, 'r', encoding='UTF-8')
+    raw_data = openf.readlines()
+    i = 0
+    if check_answer(file):
+        while i < len(raw_data):
+            if raw_data[i][0] == '$':
+                if check_meta(raw_data[i+4]):
+                    createtime = raw_data[i][1:-1].strip()
+                    enduptime = raw_data[i+1].strip()
+                    sexuality = raw_data[i+2].strip()
+                    linkdata  = raw_data[i+3].strip()
+                    string    = raw_data[i+4].strip()
+                    answers.append({'create time':createtime, 'endup time':enduptime,'sexuality':sexuality,'link':linkdata,'answer':string})
+                    i += 5
                 else:
                     i += 1
-        else:
-            articles.append({'title':file,'article':openf.readlines()})
-    answers = sorted(answers,key=lambda tm:(tm['create time']))
+            else:
+                i += 1
+    else:
+        article = {'title':file,'article':openf.readlines()}
+        articles.append(article)
 
-    outputf = open(NLPd_path + 'result.json','w',encoding='UTF-8')
-    outputf.write('[')
-    for i in range(0,len(answers)):
-        outputf.write(answers[i].__str__())
-        if i != len(answers)-1:
-            outputf.write(',\n')
-    outputf.write(']')
+
+# 输出和时间（创建和更新），性别，网址绑定的回答 Json 文件
+def time_answers(answers):
     for answer in answers:
         answer['create time'] = time.ctime(int(answer['create time']))
         answer['endup time'] = time.ctime(int(answer['endup time']))
@@ -90,6 +85,9 @@ def snowNLP_analysis(files):
             outputf.write(',\n\n')
     outputf.write(']')
 
+
+# 切词，用 jieba 进行词性切分
+def cut(answers,articles):
     outputf = open(NLPd_path + 'jieba.txt','w',encoding='UTF-8')
     jieba_data = []
     jieba.load_userdict("D:\\data_analysis\\zhihuPJanalysis\\jiebadata.txt")
@@ -98,23 +96,47 @@ def snowNLP_analysis(files):
         jieba_data.append(answer['answer'])
     for article in articles:
         jieba_data.append(article['article'])
-
     cut_data = []
     n_data = []
+    v_data = []
     for data in jieba_data:
-        rawdata = jieba.lcut(str(data)).__str__()
+        rawdata = jieba.lcut(str(data))
         splitdata = pseg.cut(str(data))
         for word,flag in splitdata:
-            if  flag[0] == 'n':
+            if flag[0] == 'n':
                 n_data.append(word)
+            if flag[0] == 'v':
+                v_data.append(word)
         for word in rawdata:
             if word not in stoplist:
                 cut_data.append(word)
                 outputf.write(word.__str__() + ' ')
-    cutwords = dict(Counter(n_data))
-    outputwords_sorted = sorted(cutwords.items(), key= lambda x : x[1], reverse=True)[:100]
-    print(outputwords_sorted)
+    return n_data,v_data
 
+
+# 用 Counter 进行词性的词频统计
+def word_num(data,flag):
+    cutwords = dict(Counter(data))
+    outputwords_sorted = sorted(cutwords.items(), key= lambda x : x[1], reverse=True)[:500]
+    f = open(NLPd_path + flag + '_num.txt','w',encoding='UTF-8')
+    f.write(outputwords_sorted.__str__())
+
+
+# 主要处理函数，用到了上面的所有辅助函数
+def snowNLP_analysis(files):
+    answers = []
+    articles = []
+    for file in files:
+        get_data(file,answers,articles)
+
+    answers = sorted(answers,key=lambda tm:(tm['create time']))
+
+    time_answers(answers)
+
+    n_data,v_data = cut(answers,articles)
+
+    word_num(n_data,'n')
+    word_num(v_data,'v')
 
 
 if __name__ == "__main__":
